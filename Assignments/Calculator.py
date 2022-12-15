385b566e719580c092749b7c5d929f26c0ee9ac2
Darryl B <dbuma2024@acrss.ca>
2022-12-15 11:37:16 -0800

    test

---
+++ Assignments/Calculator.py
@@ -0,0 +1,19 @@
+# radical function
+
+num1 = int(input("Enter a number: "))
+function = input("Enter a function: ") # use list
+num2 = int(input("Enter another number: "))
+
+def add(num):
+    num1 + num2 == added_num
+    return added_num
+
+def sub(subtracted_num):
+    num1 - num2 == subtracted_num
+    return subtracted_num
+
+def power(power_num):
+    num1 ** num2 == power_num
+    return power_num
+
+
