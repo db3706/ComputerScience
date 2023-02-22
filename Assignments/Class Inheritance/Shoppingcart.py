7f176e08f32ad0ee02d62dfb4e62834684589dd7
Darryl B <dbuma2024@acrss.ca>
2023-02-22 14:43:45 -0800

    shoppingcart

--- Assignments/Class Inheritance/Shoppingcart.py
+++ Assignments/Class Inheritance/Shoppingcart.py
@@ -1,2 +1,20 @@
-class ShoppingCart():
-    items_in_cart = {}
+items_in_cart = {
+        "product": "price"
+        }
+
+class ShoppingCart(object):
+
+    
+    def __init__(self, customer_name):
+        self.customer_name = customer_name
+
+    # https://www.w3schools.com/python/python_dictionaries_add.asp
+    def add_item(self, product, price):
+        items_in_cart[product] = price
+        print(f"{product} added to shopping cart")
+
+    def remove_item(self, product, price):
+
+print(items_in_cart)
+
+https://www.scaler.com/topics/remove-key-from-dictionary-python/
