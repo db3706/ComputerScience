b531b6e306ad10d0fa582a99b738d33b1c7fc318
Darryl B <dbuma2024@acrss.ca>
2023-02-24 13:56:18 -0800

    assign

--- Assignments/Class Inheritance/Shoppingcart.py
+++ Assignments/Class Inheritance/Shoppingcart.py
@@ -1,10 +1,7 @@
-items_in_cart = {
-        "product": "price"
-        }
+items_in_cart = {}
 
-class ShoppingCart(object):
+class ShoppingCart():
 
-    
     def __init__(self, customer_name):
         self.customer_name = customer_name
 
@@ -14,7 +11,19 @@ class ShoppingCart(object):
         print(f"{product} added to shopping cart")
 
     def remove_item(self, product, price):
+        print("Placeholder")
 
-print(items_in_cart)
+# https://www.scaler.com/topics/remove-key-from-dictionary-python/
 
-https://www.scaler.com/topics/remove-key-from-dictionary-python/
+
+class Apple(ShoppingCart):
+    def add_item(self, product="apple", price=22):
+        return super().add_item(product, price)
+
+
+
+
+items_in_cart["apple"] = 40
+
+apple = Apple("Bro harris")
+print(apple)
